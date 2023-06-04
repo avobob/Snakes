bbcd5f0cd5790399bc0a3188c3f78e6f8287eb11
wyatt <wyattvanwag@gmail.com>
2023-06-04 11:26:28 +1000

    Revert "actually finished and done"
    This reverts commit 560d6e413107c761c7be4d5be8c88c431f70ba12.

--- screen.h
+++ screen.h
@@ -1,8 +1,10 @@
 #pragma once
-#include <vector>
 #include <string>
+<<<<<<< HEAD
 #include "move_snake.h"
 #include "yellow_box.h"
+=======
+>>>>>>> parent of 560d6e4... actually finished and done
 #include "raylib.h"
 using namespace std;
 class screen
@@ -14,13 +16,60 @@ public:
 	void Load();
 	void Unload();
 	void Border();
+<<<<<<< HEAD
+=======
+	void Body();
+	//void BodyColour();
+	//void DrawPixel(int posX, int posY, string color);
+
+>>>>>>> parent of 560d6e4... actually finished and done
 	void Update(float deltaTime);
 	void Draw();
 	//Color GetTileColor(int tileValue);
 
+<<<<<<< HEAD
 public:
 	storage ST;
 	move_snake snake;
 	yellow_box yellow;
+=======
+private:
+	int fps = 60;
+	int fpsCounter = 0;
+	int frameAmount = 21;
+	int TOD = 0;
+	int whiteBox = 0;
+	int greenBox = 391;
+	int redBox = 364;
+	int yellowBox = 0;
+	int tempBox;
+	const char* GameOver = nullptr;
+	int GameCounter = -1;
+	int Counter = 21;
+	int body = Counter;
+
+	bool yellow = false;
+	bool GameEnd = false;
+
+	int Key = 0;
+	int Move = 0;
+
+	int m_windowWidth = 810;
+	int m_windowHeight = 810;
+
+	static const int ROWS = 27;
+	static const int COLS = 27;
+
+	static const int ROWS2 = 21;
+	static const int COLS2 = 1;
+
+	int m_tiles[ROWS * COLS];
+
+	int m_tileWidth = 30;
+	int m_tileHeight = 30;
+
+	int b_tiles[ROWS2 * COLS2];
+
+>>>>>>> parent of 560d6e4... actually finished and done
 };
 
